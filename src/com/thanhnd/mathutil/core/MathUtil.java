28df8dcdeb0413599e603286e7b773fcb64301ae
thanhnd <nguyenthanh31102005@gmail.com>
2026-02-17 16:53:46 +0700

    refactor the main code and green 4ever

--- src/com/thanhnd/mathutil/core/MathUtil.java
+++ src/com/thanhnd/mathutil/core/MathUtil.java
@@ -9,18 +9,34 @@ package com.thanhnd.mathutil.core;
  * @author Admin
  */
 public class MathUtil {
+//    public static long getFactorial(int n){
+//        if(n <0 || n >20){
+//            throw  new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//            
+//        }
+//        if(n == 0|| n == 1){
+//            return 1;
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    
+    
+    
     public static long getFactorial(int n){
-        if(n <0 || n >20){
+      
+        
+                if(n <0 || n >20){
             throw  new IllegalArgumentException("Invalid argument. N must be between 0..20");
             
         }
         if(n == 0|| n == 1){
             return 1;
         }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        
+        return n * getFactorial(n - 1);
     }
 }
